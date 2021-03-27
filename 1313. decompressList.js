41d8f210e74f72a227b68649e36aeb1ab84ee048
Tian <skyjunechentian@sina.com>
2021-03-27 20:45:59 +0800

    fix

--- 1313. decompressList.js
+++ 1313. decompressList.js
@@ -1,10 +1,10 @@
 let decompressRLElist = function test(nums) {
   const result = [];
-  for (let i = 0; i < nums.length / 2; i++) {
-    for (let j = 0; j < nums[i + 1]; j++) {
-      result.push(nums[i]);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums[i]; j++) {
+      result.push(nums[i + 1]);
     }
-    i = +1;
+    i += 1;
   }
   return result;
 };
