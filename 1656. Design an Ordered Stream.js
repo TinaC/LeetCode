dea22bd74cbe63d7c90f3ce0a8827d43e38eab5b
Tian <skyjunechentian@sina.com>
2021-04-03 11:22:47 +0800

    fix

--- 1656. Design an Ordered Stream.js
+++ 1656. Design an Ordered Stream.js
@@ -1,6 +1,9 @@
+/**
+ * @param {number} n
+ */
 const OrderedStream = function f(n) {
   this.stream = new Map();
-  this.pointer = 0;
+  this.pointer = 1;
 };
 
 /**
@@ -11,16 +14,25 @@ const OrderedStream = function f(n) {
 OrderedStream.prototype.insert = function f(idKey, value) {
   this.stream.set(idKey, value);
   const result = [];
+
   if (idKey === this.pointer) {
-    const next = idKey + 1;
-    
-    while (this.stream.has(next)) {
-      result.push(this.stream.get(next));
-      this.pointer = next;
+    result.push(value);
+    this.pointer++;
+    let i = idKey + 1;
+    while (this.stream.has(i)) {
+      result.push(this.stream.get(i));
+      i++;
+      this.pointer++;
     }
   }
   return result;
 };
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */
 
 const obj = new OrderedStream(5);
 console.log(obj.insert(3, 'ccccc'));
